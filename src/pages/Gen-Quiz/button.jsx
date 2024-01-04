0cf917fe6d2d327a16b9c603c333d4fbabcfb269
Naman Sarda <namansarda2007@gmail.com>
2024-01-04 16:45:30 +0530

    commit

--- src/pages/Gen-Quiz/button.jsx
+++ src/pages/Gen-Quiz/button.jsx
@@ -1,48 +1,65 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import "../../styles/tailwind.css";
+import { useNavigate } from "react-router";
+import "./button.css";
 
 const FileUploadButton = ({ label, onFileUpload }) => {
-	const [isModalOpen, setModalOpen] = useState(false);
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [isButtonHidden, setButtonHidden] = useState(false);
 
-	const handleButtonClick = () => {
-		setModalOpen(true);
-	};
+  const handleButtonClick = () => {
+    setModalOpen(true);
+    setButtonHidden(true);
+  };
 
-	const handleFileUpload = (event) => {
-		const uploadedFile = event.target.files[0];
-		onFileUpload(uploadedFile);
-		setModalOpen(false);
-	};
+  const handleFileUpload = (event) => {
+    const uploadedFile = event.target.files[0];
+    onFileUpload(uploadedFile);
+  };
 
-	return (
-		<div>
-			<button className="mt-4" onClick={handleButtonClick}>
-				{label}
-			</button>
+  const navigate = useNavigate();
+  const redirectfile = () => {
+    setButtonHidden(false); // Show the button again when redirecting
+    return navigate("/home/play-quiz");
+  };
 
-			{isModalOpen && (
-				<div className="flex flex-row items-center justify-center">
-					<span
-						className="close flex ml-auto"
-						onClick={() => setModalOpen(false)}
-					>
-						&times;
-					</span>
-					<input
-						type="file"
-						className="flex ml-auto"
-						onChange={handleFileUpload}
-					/>
-				</div>
-			)}
-		</div>
-	);
+  return (
+    <div className="button-box">
+      <button
+        className={`mt-4 label-but ${isButtonHidden ? "hidden" : ""}`}
+        onClick={handleButtonClick}
+      >
+        {label}
+      </button>
+
+      {isModalOpen && (
+        <>
+          <div className="flex flex-row items-center justify-center select-cont">
+            <span
+              className="close flex input1"
+              onClick={() => setModalOpen(false)}
+            >
+              &times;
+            </span>
+            <input
+              type="file"
+              className="flex  input1"
+              onChange={handleFileUpload}
+            />
+          </div>
+          <button className="create-but" onClick={redirectfile}>
+            Create Quiz
+          </button>
+        </>
+      )}
+    </div>
+  );
 };
 
 FileUploadButton.propTypes = {
-	label: PropTypes.string.isRequired,
-	onFileUpload: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+  onFileUpload: PropTypes.func.isRequired,
 };
 
 export default FileUploadButton;
