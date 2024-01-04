63aa399b409b9f11a594012f2f62d4c319ca9056
Manas8803 <114303599+Manas8803@users.noreply.github.com>
2024-01-04 13:35:05 +0530

    Feat : Linked genQuiz route

---
+++ src/pages/pdfquiz/button.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const FileUploadButton = ({ label, onFileUpload }) => {
+	const [isModalOpen, setModalOpen] = useState(false);
+
+	const handleButtonClick = () => {
+		setModalOpen(true);
+	};
+
+	const handleFileUpload = (event) => {
+		// Perform any necessary file upload logic here
+		const uploadedFile = event.target.files[0];
+		onFileUpload(uploadedFile);
+		setModalOpen(false);
+	};
+
+	return (
+		<div>
+			<button className="mt-4" onClick={handleButtonClick}>
+				{label}
+			</button>
+
+			{isModalOpen && (
+				<div className="modal">
+					<div className="modal-content">
+						<span className="close" onClick={() => setModalOpen(false)}>
+							&times;
+						</span>
+						<input type="file" onChange={handleFileUpload} />
+					</div>
+				</div>
+			)}
+		</div>
+	);
+};
+
+FileUploadButton.propTypes = {
+	label: PropTypes.string.isRequired,
+	onFileUpload: PropTypes.func.isRequired,
+};
+
+export default FileUploadButton;
